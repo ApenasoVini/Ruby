ff80680e0dd27cfaaf4e3534bdcc204b6b162c53
Vinicius Soares Franca <vi058587@intelbras.com.br>
2024-02-26 16:12:06 -0300

    calculadora

--- calculadora.rb
+++ calculadora.rb
@@ -1,56 +1,59 @@
 loop do
+  puts 'Esta é uma calculadora em Ruby! Escolha um dos cálculos abaixo:'
+  puts 'Adição (1)'
+  puts 'Subtração (2)'
+  puts 'Multiplicação (3)'
+  puts 'Divisão (4)'
+  puts 'Potenciação (5)'
+  puts 'Sair (6)'
+  print 'Digite o número do cálculo desejado: '
+  calculo = gets.chomp.to_i
 
-puts 'Esta é uma calculadora em Ruby! Escolha um dos cálculos abaixo:'
-puts 'Adição (1)'
-puts 'Subtração (2)'
-puts 'Multiplicação (3)'
-puts 'Divisão (4)'
-puts 'Potenciação (5)'
+  case calculo
+  when 1
+    print 'Digite o primeiro número: '
+    num1 = gets.chomp.to_f
+    print 'Digite o segundo número: '
+    num2 = gets.chomp.to_f
+    resultado = num1 + num2
+    puts "O resultado é #{resultado}"
 
-print 'Digite o número do cálculo desejado:'
-calculo = gets.chomp_io
+  when 2
+    print 'Digite o primeiro número: '
+    num1 = gets.chomp.to_f
+    print 'Digite o segundo número: '
+    num2 = gets.chomp.to_f
+    resultado = num1 - num2
+    puts "O resultado é #{resultado}"
 
-case calculo
-when 1
-  print 'Digite o primeiro número: '
-  num1 = gets.chomp_io
-  print 'Digite o primeiro número: '
-  num2 = gets.chomp_io
-  resultado = num1 + num2
-  puts "O resultado é #{resultado}"
+  when 3
+    print 'Digite o primeiro número: '
+    num1 = gets.chomp.to_f
+    print 'Digite o segundo número: '
+    num2 = gets.chomp.to_f
+    resultado = num1 * num2
+    puts "O resultado é #{resultado}"
 
-when 2
-  print 'Digite o primeiro número: '
-  num1 = gets.chomp_io
-  print 'Digite o primeiro número: '
-  num2 = gets.chomp_io
-  resultado = num1 - num2
-  puts "O resultado é #{resultado}"
+  when 4
+    print 'Digite o primeiro número: '
+    num1 = gets.chomp.to_f
+    print 'Digite o segundo número: '
+    num2 = gets.chomp.to_f
+    resultado = num1 / num2
+    puts "O resultado é #{resultado}"
 
-when 3
-  print 'Digite o primeiro número: '
-  num1 = gets.chomp_io
-  print 'Digite o primeiro número: '
-  num2 = gets.chomp_io
-  resultado = num1 * num2
-  puts "O resultado é #{resultado}"
+  when 5
+    print 'Digite a base: '
+    base = gets.chomp.to_f
+    print 'Digite o expoente: '
+    expoente = gets.chomp.to_f
+    resultado = base ** expoente
+    puts "O resultado é #{resultado}"
 
-when 4
-  print 'Digite o primeiro número: '
-  num1 = gets.chomp_io
-  print 'Digite o primeiro número: '
-  num2 = gets.chomp_io
-  resultado = num1 / num2
-  puts "O resultado é #{resultado}"
-
-when 5
-  print 'Digite o primeiro número: '
-  num1 = gets.chomp_io
-  print 'Digite o primeiro número: '
-  num2 = gets.chomp_io
-  resultado = num1 ** num2
-  puts "O resultado é #{resultado}"
-
-else
-  puts 'Opção inválida'
+  when 6
+    break
+  else
+    puts 'Opção inválida'
+  end
+  system "cls"
 end
