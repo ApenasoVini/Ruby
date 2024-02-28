17cf3e1e2ee2422131ff6c9be698f0c57147ee24
Vinicius Soares Franca <vi058587@intelbras.com.br>
2024-02-28 16:27:04 -0300

    Ruby

--- gems.rb
+++ gems.rb
@@ -1,3 +1,4 @@
+#GEM OS
 require 'os'
 
 def sistem
@@ -13,3 +14,18 @@ def sistem
 end
 
 puts "Meu PC possui #{OS.cpu_count} cores, é #{OS.bits} bits e o sistema operacional é #{sistem}"
+
+
+#GEM CPF_CNPJ
+require 'cpf_cnpj'
+
+def validation
+  cpf = "909.909.909-90"
+  if CPF.valid?(cpf)
+    "CPF existente!"
+  else
+    "CPF inexistente!"
+  end
+end
+
+puts validation
