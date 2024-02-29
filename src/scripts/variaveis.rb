57cbafc82c14ccd131f817b781401fd4d00289ec
Vinicius Soares Franca <vi058587@intelbras.com.br>
2024-02-29 15:12:22 -0300

    Variaveis

---
+++ src/scripts/variaveis.rb
@@ -0,0 +1,49 @@
+# Local
+def local
+  var = 'Variável local'
+  puts var
+end
+local
+
+puts local #Erro
+
+
+# Global
+class Global
+  def global
+    $x = 0
+    puts $x
+  end
+end
+
+class Change
+  def change
+    $x += 1
+    puts $x
+  end
+end
+
+global = Global.new
+global.global
+
+change = Change.new
+change.change
+change.change
+
+puts $x
+
+# Var de Classe
+class User
+  @@user_count = 0
+  def add(name)
+    puts "User #{name} adicionado"
+    @@user_count += 1
+    puts @@user_count
+  end
+end
+
+first_user = User.new
+first_user.add('Vini')
+
+second_user = User.new
+second_user.add('Mateus')
