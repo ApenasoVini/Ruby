f21ae319cd921193df7a529b7d2a92d49f3c6ad2
Vinicius Soares Franca <vi058587@intelbras.com.br>
2024-02-29 14:57:15 -0300

    Require

---
+++ src/scripts/app/main.rb
@@ -0,0 +1,11 @@
+require_relative 'classe'
+require_relative 'classeDois.rb'
+
+animal = Animal.new
+puts 'Animal:'
+animal.pular
+puts '---'
+puts 'cachorro:'
+cachorro = Cachorro.new
+cachorro.pular
+cachorro.latir
